0fff0cdd0a381a940909c748341d7cf910cfb51a
Michael Beardsworth <michael.beardsworth@gmail.com>
2013-09-17 22:59:35 -0700

    Got graph working

--- scripts/Communication.py
+++ scripts/Communication.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 import rospy
-from crazyfly.msg import *
+from std_msgs.msg import *
+from crazyflie.msg import *
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.utils import callbacks
 from cfclient.utils.logconfigreader import LogConfig
 from cfclient.utils.logconfigreader import LogVariable
+
+from threading import Thread
 
 class Communication:
 
@@ -28,16 +31,71 @@ class Communication:
     self.crazyflie.connectionLost.add_callback(self.connectionLost)
 
 
+    self.acc_pub = rospy.Publisher("/crazyflie/Acceleration", Acceleration)
+    self.ori_pub = rospy.Publisher("/crazyflie/Orientation", Orientation)
+
   def connectSetupFinished(self, linkURI):
     rospy.loginfo("Connected on %s"%linkURI)
 
-    #Set up pitch/yaw/roll logging (should probably be in a separate function)
-    #period_msec = 10
+    #set up logging
+    period_msec = 10
+    acc_conf = LogConfig("Accel", period_msec)
+    acc_conf.addVariable(LogVariable("acc.x", "float"))
+    acc_conf.addVariable(LogVariable("acc.y", "float"))
+    acc_conf.addVariable(LogVariable("acc.z", "float"))
+  
+    ori_conf = LogConfig("Stabilizer", period_msec)
+    ori_conf.addVariable(LogVariable("stabilizer.pitch", "float"))
+    ori_conf.addVariable(LogVariable("stabilizer.yaw", "float"))
+    ori_conf.addVariable(LogVariable("stabilizer.roll", "float"))
+    ori_conf.addVariable(LogVariable("stabilizer.thrust", "float"))
 
-    #self.logconf = LogConfig("Logging", period_msec)
 
-  def connectionLost(self, linkURI):
-    rospy.loginfo("Connection lost on %s"%linkURI)
+
+    #Set up acc logging
+    self.acc_log = self.crazyflie.log.create_log_packet(acc_conf)
+    self.orientation_log = self.crazyflie.log.create_log_packet(ori_conf)
+  
+
+    if (self.acc_log != None):
+      self.acc_log.dataReceived.add_callback(self.acc_data_callback)
+      self.acc_log.start()
+      rospy.loginfo("Succesfully set up acc logging")
+    else:
+      rospy.logerr("Unable to set up acc logging")
+
+    if (self.orientation_log != None):
+      self.orientation_log.dataReceived.add_callback(self.orientation_data_callback)
+      self.orientation_log.start()
+      rospy.loginfo("Succesfully set up stabilizer logging")
+    else:
+      rospy.logerr("Unable to set up stabilizer logging")
+
+
+
+  def connectionLost(self, linkURI, errmsg):
+    rospy.loginfo("Connection lost on %s: %s"%(linkURI, errmsg))
+    exit()
     
   def connectionFailed(self, linkURI):
     rospy.logerr("Connection failed on %s"%linkURI)
+    exit()
+
+  def acc_data_callback(self, data):
+    #print data
+    #rospy.loginfo(str(data))
+    self.acc_pub.publish(data["acc.x"], data["acc.y"], data["acc.z"])
+  
+  def orientation_data_callback(self, data):
+    #print data["stabilizer.pitch"], data["stabilizer.yaw"], data["stabilizer.roll"], data["stabilizer.thrust"]
+    self.ori_pub.publish(data["stabilizer.pitch"], data["stabilizer.yaw"], data["stabilizer.roll"], data["stabilizer.thrust"])
+    #rospy.loginfo(str(data))
+
+
+if __name__ == '__main__':
+  rospy.init_node('communication')
+
+  comms = Communication()
+
+  while not rospy.is_shutdown():
+    rospy.sleep(1.0)
